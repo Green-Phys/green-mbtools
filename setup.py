501016ee79973d8c6f1026d8dd6370c9424d2022
Gaurav Harsha <gauravharsha05@gmail.com>
2022-04-11 01:12:20 -0400

    add setup.py for mbanalysis package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from numpy.distutils.core import setup
+
+
+setup(
+   name='mbanalysis',
+   version='1.0',
+   description="A package for post processing of finite-temperature \
+       Green's function and self-energy data",
+   packages=['mbanalysis', 'mbanalysis.src'],
+)
