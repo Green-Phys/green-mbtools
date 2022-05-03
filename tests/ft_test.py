b22e5f9d0dae2667f41f2d313363f40a10764141
Gaurav Harsha <gauravharsha05@gmail.com>
2022-05-02 23:09:27 -0400

    add data and tests for real to reciprocal ft.py

---
+++ tests/ft_test.py
@@ -0,0 +1,41 @@
+from mbanalysis.src.ft import construct_rmesh,\
+    compute_fourier_coefficients, k_to_real, real_to_k
+import numpy as np
+
+
+def test_fourier_transform():
+    """Test function for Fourier Transform functions.
+    FT between real and reciprocal space.
+    """
+
+    # Read test overlap matrices
+    Sk10 = np.load('/data/Sk10.npy')
+    Sk6 = np.load('/data/Sk6.npy')
+
+    # Get k-mesh
+    kmesh_scaled_nk10 = np.load('/data/kmesh_k10.npy')
+    kmesh_scaled_nk6 = np.load('/data/kmesh_k6.npy')
+
+    # Get number of k-points
+    nk6 = int(np.cbrt(Sk6.shape[0]))
+    nk10 = int(np.cbrt(Sk10.shape[0]))
+
+    # Check Fourier transformation for nk = 6
+    rmesh_nk6 = construct_rmesh(nk6, nk6, nk6)
+    fkr_nk6, frk_nk6 = compute_fourier_coefficients(
+        kmesh_scaled_nk6, rmesh_nk6
+    )
+    Si6 = k_to_real(frk_nk6, Sk6, [1]*kmesh_scaled_nk6.shape[0])
+    Sk6_check = real_to_k(fkr_nk6, Si6)
+    diff = Sk6_check - Sk6
+    assert np.max(np.abs(diff)) < 1e-8
+
+    # Check Fourier transformation for nk = 10
+    rmesh_nk10 = construct_rmesh(nk10, nk10, nk10)
+    fkr_nk10, frk_nk10 = compute_fourier_coefficients(
+        kmesh_scaled_nk10, rmesh_nk10
+    )
+    Si10 = k_to_real(frk_nk10, Sk10, [1]*kmesh_scaled_nk10.shape[0])
+    Sk10_check = real_to_k(fkr_nk10, Si10)
+    diff = Sk10_check - Sk10
+    assert np.max(np.abs(diff)) < 1e-8
