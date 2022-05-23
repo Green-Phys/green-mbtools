6508d4f10974a7255b3f2c1ff7bd1adcee9883d1
Gaurav Harsha <gauravharsha05@gmail.com>
2022-05-23 17:51:40 -0400

    update setup.py to exclude data files in install

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from numpy.distutils.core import setup
 
 setup(
    name='mbanalysis',
-   version='1.0',
+   version='1.1',
    description="A package for post processing of finite-temperature \
        Green's function and self-energy data",
-   packages=['mbanalysis', 'mbanalysis.src', 'mbanalysis.data'],
+   packages=['mbanalysis', 'mbanalysis.src'],
    package_data={
        "mbanalysis.data": ["*.h5", "*.npy"]
    }
