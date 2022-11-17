3e3ed75a516b4612493f887f2b8309e85552b617
Gaurav Harsha <gauravharsha05@gmail.com>
2022-11-17 14:21:49 -0500

    update setup py and library links for Nevanlinna

--- setup.py
+++ setup.py
@@ -5,21 +5,27 @@ from numpy.distutils.core import setup, Extension
 nevanlinna = Extension(
     'mbanalysis.nevanlinna',
     sources=['Nevanlinna/nevanlinna.cpp', ],
-    include_dirs=['/usr/include/eigen3', ],
-    libraries=['gmp', 'gmpxx']
+    include_dirs=[
+        '/usr/local/include',
+        '/usr/local/include/eigen3',
+    ],
+    libraries=['gmp', 'gmpxx', 'mpfr']
 )
 
 # Build Caratheodory extension
 caratheodory = Extension(
     'mbanalysis.caratheodory',
     sources=['Caratheodory/caratheodory.cpp', ],
-    include_dirs=['/usr/include/eigen3', ],
-    libraries=['gmp', 'gmpxx', 'mpfr']
+    include_dirs=[
+        '/usr/local/include',
+        '/usr/local/include/eigen3',
+    ],
+    libraries=['gmp', 'gmpxx', 'mpfr'],
 )
 
 setup(
    name='mbanalysis',
-   version='1.2',
+   version='1.2.1',
    description="A package for post processing of finite-temperature \
        Green's function and self-energy data",
    packages=['mbanalysis', 'mbanalysis.src'],
